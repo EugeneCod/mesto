2ca34760a5bb925b529578e8478ec7939c2f72ef
Eugene Korobkin <eugene_korobkin@mail.ru>
2022-05-28 20:30:48 +0300

    feat: add initial cards and form markup

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,11 @@ const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 const formEditProfile = document.querySelector('.edit-profile');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about-self');
-let inputName = document.querySelector('.edit-profile__input-line_assignment_name');
-let inputAbout = document.querySelector('.edit-profile__input-line_assignment_about-self');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about-self');
+const inputName = document.querySelector('.edit-profile__input-line_assignment_name');
+const inputAbout = document.querySelector('.edit-profile__input-line_assignment_about-self');
+
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -24,6 +25,57 @@ function closePopup () {
   popup.classList.remove('popup_opened')
 }
 
+document.addEventListener("DOMContentLoaded", addOriginalCards)
+
 editButton.addEventListener ('click', openPopup);
 closeButton.addEventListener ('click', closePopup);
 formEditProfile.addEventListener('submit', formSubmitHandler);
+
+
+
+/* --------------Функционал добавления карточек-------------- */
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+
+function addOriginalCards() {
+  initialCards.forEach(function (element) {
+    const cardTemplate = document.querySelector('#card-template');
+    const cardElement = cardTemplate.content.cloneNode(true);
+    const cardContainer = document.querySelector('.elements');
+    const imageElement = cardElement.querySelector('.elements__image');
+
+    imageElement.setAttribute('src', `${element.link}`);
+    imageElement.setAttribute('alt', `${element.name}`);
+    cardElement.querySelector('.elements__location').textContent = element.name;
+
+    cardContainer.prepend(cardElement);
+  }
+)};
+
+// addOriginalCards();
